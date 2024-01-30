9cd04e109061f28ae88e5995cd1a54c460c21080
Josue Heredia <josue-heredia@outlook.com>
2024-01-30 20:57:12 +0000

    completed all except test

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+
+    @question = params[:answer]
+
+    if @question.include?('?')
+      @response = 'Silly question, get dressed and go to work!'
+    elsif @question.include?('I am going to work')
+      @response = 'Great!'
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
